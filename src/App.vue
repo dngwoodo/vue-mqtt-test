4f7c3352f5e6b7727f5e2458f9aba8a81d492294
dngwoodo <dngwoodo@gmail.com>
2021-02-03 14:27:50 +0900

    etc: init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <mqtt-connection-form />
+  </div>
+</template>
+
+<script>
+import MqttConnectionForm from "./components/MqttConnectionForm.vue";
+export default {
+  name: "App",
+  components: { MqttConnectionForm }
+};
+</script>
---
+++ src/components/MqttConnectionForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <el-card shadow="always" style="margin-bottom:30px;">
+      <mqtt-configuration-form />
+    </el-card>
+    <el-card shadow="always" style="margin-bottom:30px;">
+      <mqtt-subscribe-form />
+    </el-card>
+  </div>
+</template>
+
+<script>
+import MqttConfigurationForm from "./MqttConfigurationForm.vue";
+import MqttSubscribeForm from "./MqttSubscribeForm.vue";
+export default {
+  components: { MqttConfigurationForm, MqttSubscribeForm }
+};
+</script>
+
+<style lang="scss">
+@import "~@/styles";
+</style>
